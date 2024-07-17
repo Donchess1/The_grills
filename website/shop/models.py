c7e7481d64ee5efa4e87f36bb20e867fe73a3b2b
Donchess1 <stepabod@.com>
2024-07-17 15:56:11 +0000

    shop and home page linked

---
+++ website/shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    products = models.ManyToManyField(Product, through='CartItem')
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
