fabd62dbff6416cb9333f0893506d28814267b10
Donchess1 <stepabod@.com>
2024-07-30 18:40:23 +0000

    checkpoint

--- website/shop/apps.py
+++ website/shop/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class ShopConfig(AppConfig):
     name = 'shop'
 
-     def ready(self):
+    def ready(self):
         import shop.signals
 
 class GrillzConfig(AppConfig):
--- website/website/settings.py
+++ website/website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nsd4ej4z%j-0je6uh9nx=c*2@(t25lkby5w6w(c(=$rsw3$^hu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
